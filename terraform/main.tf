d52a78aaa83bc462660829f6b70b1a7d7f78af82
balajigan <balaji_inf@yahoo.com>
2023-01-02 17:23:48 -0600

    Update main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,1 +1,27 @@
 # Terraform main file
+
+provider "google" {
+  project 	= "mytemporaryproject28490"
+# credentials 	= "${file("credentials.json")}"
+  region	= "us-central1"
+  zone		= "us-central1-a"
+}
+
+resource "google_compute_instance" "my_instance" {
+  name		= "terraform-instance-02"
+  machine_type	= "f1-micro"
+  zone		= "us-central1-a"	
+  allow_stopping_for_update	= true
+
+  boot_disk {
+	initialize_params {
+		image = "debian-10-buster-v20220719"
+	}   	
+   }
+  network_interface {
+	network = "default"
+	access_config {
+
+	}
+ }
+}
