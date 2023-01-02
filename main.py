09e073e350505ebfb753695dd774061a779cfd7c
balajigan <balaji_inf@yahoo.com>
2023-01-02 17:53:05 -0600

    Update main.py

--- main.py
+++ main.py
@@ -8,7 +8,7 @@ print(os.environ)
 # read by default sheet of an excel file
 dataframe1 = pd.read_excel('sample.xlsx')
 
-f = open("terraform_commands.txt", "w")
+f = open("terraform/terraform_commands.txt", "w")
 f.write("This file is newly created ...")
 f.close()
 
