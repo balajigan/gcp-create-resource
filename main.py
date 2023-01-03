bbc1af332236acd1b48a8f488af9ea7ac8288dd1
balajigan <balaji_inf@yahoo.com>
2023-01-03 15:35:34 -0600

    Update main.py

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
 
 import pandas as pd
 import os
+# Import the Secret Manager client library.
+from google.cloud import secretmanager
+
 
 print ("main.py started ...")
 print(os.environ)
@@ -11,7 +14,15 @@ dataframe1 = pd.read_excel('sample.xlsx')
 f = open("terraform/terraform_commands.txt", "w")
 f.write("This file is newly created ...")
 f.close()
-
 print(dataframe1)
+
+# Create the Secret Manager client.
+client = secretmanager.SecretManagerServiceClient()
+
+# Access the secret version.
+response = client.access_secret_version(request={"name": "app-username"})
+payload = response.payload.data.decode("UTF-8")
+print("Plaintext: {}".format(payload))
+
 
 
