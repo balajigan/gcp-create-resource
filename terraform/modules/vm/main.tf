5ae74a7448d21f0085cd3c49ac7ed0b60810129f
balajigan <balaji_inf@yahoo.com>
2023-01-04 13:37:30 -0600

    Update main.tf

--- terraform/modules/vm/main.tf
+++ terraform/modules/vm/main.tf
@@ -1,1 +1,19 @@
 # Module for vm
+resource "google_compute_instance" "my_instance" {
+  name		= var.name	
+  machine_type	= "f1-micro"
+  zone		= "us-central1-a"	
+  allow_stopping_for_update	= true
+
+  boot_disk {
+	initialize_params {
+		image = "debian-10-buster-v20220719"
+	}   	
+   }
+  network_interface {
+	network = "default"
+	access_config {
+
+	}
+ }
+}
