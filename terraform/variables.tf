7fb6824058b9907649aeafa4967b739be4826d52
balajigan <balaji_inf@yahoo.com>
2023-01-03 18:00:49 -0600

    Update variables.tf

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -1,1 +1,14 @@
 # variables
+
+variable "project" {
+  type        = string
+  description = "The project ID of the resource"
+}
+variable "region" {
+  type        = string
+  description = "cloud region of the resource"
+}
+variable "name" {
+  type        = string
+  description = "Name of the resource to be created"
+}
