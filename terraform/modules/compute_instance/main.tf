99c910b401794416eeae97648f82f518925bf77d
balajigan <balaji_inf@yahoo.com>
2023-01-22 00:32:13 -0600

    Create main.tf

---
+++ terraform/modules/compute_instance/main.tf
@@ -0,0 +1,26 @@
+# Terraform main file
+
+provider "google" {
+  project 	= var.project_id
+  credentials 	= "${file("credentials.json")}"
+  region	= var.region_name
+  zone		= var.zone_name
+}
+
+resource "google_compute_instance" "my_instance" {
+  name		= var.instance_name
+  machine_type	= var.machine_type
+  zone		= var.zone_name
+  allow_stopping_for_update	= true
+
+ boot_disk {
+	initialize_params {
+		image = "debian-10-buster-v20220719"
+	}
+   }
+  network_interface {
+	network = "default"
+	access_config {
+	}
+ }
+}
