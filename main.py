a56e0a2f92bbe7be02f3d49cab367382fb16980f
balajigan <balaji_inf@yahoo.com>
2023-02-27 19:34:40 -0600

    Update main.py

--- main.py
+++ main.py
@@ -5,6 +5,11 @@ import shutil
 # Import the Secret Manager client library.
 from google.cloud import secretmanager
 
+def isNotBlank (myString):
+    return bool(myString and myString.strip())
+def isNaN(num):
+    return num != num
+
 print ("main.py started ...")
 
 # Create the Secret Manager client....
@@ -19,40 +24,51 @@ credentialFile = open("terraform/credentials.json", "w")
 credentialFile.write(payload)
 credentialFile.close()
 
-values_auto_file_name = 'terraform/values.auto.tfvars'
-tf_module_file_path = 'terraform/modules/'
+values_auto_file_name = 'terraform\\values.auto.tfvars'
+tf_module_file_path = 'terraform\\modules\\'
 xls = pd.ExcelFile('Infra_variable_sheet.xlsx')
-print(xls.sheet_names[0])
-dataframe1 = pd.read_excel('Infra_variable_sheet.xlsx')
-print("Raw dataframe = ")
-print(dataframe1)
 
-src1 = tf_module_file_path + xls.sheet_names[0] + '/main.tf'
-src2 = tf_module_file_path + xls.sheet_names[0] + '/variables.tf'
-dst1 = 'terraform/main.tf'
-dst2 = 'terraform/variables.tf'
+print(xls.sheet_names)
 
-shutil.copyfile(src1, dst1)
-shutil.copyfile(src2, dst2)
+for resource_type in xls.sheet_names:
+    print("")
+    print(resource_type)
+    dataframe1 = pd.read_excel('Infra_variable_sheet.xlsx', sheet_name=resource_type, converters={'Parameter Name':str,'Values':str})
 
-f = open(values_auto_file_name, "w")
-for index, row in dataframe1.iterrows():
-    if(row['isMandatory'] == 'yes'):
-        param = row['Parameter Name'] + '  =   '
-        if(row['Data Type'] == 'string'):
-            param = param + '"' + row['Values'] + '"'
-            print(param)
-            f.writelines(param + "\n")
-        elif(row['Data Type'] == 'map(string)'):
-            mapStringList = row['Values'].strip()[1:-1].split('\n')
-            for item in mapStringList:
-                if '=' in item:
-                    param = row['Parameter Name'] + '_' + item.strip()
-                    print(param)
-                    f.writelines(param + "\n")
-        else:
-            param = param + row['Values']
-            print(param)
-            f.writelines(param + "\n")
-f.close()
+    src1 = tf_module_file_path + resource_type + '\\main.tf'
+    src2 = tf_module_file_path + resource_type + '\\variables.tf'
+    dst1 = 'terraform\\main.tf'
+    dst2 = 'terraform\\variables.tf'
+
+    shutil.copyfile(src1, dst1)
+    shutil.copyfile(src2, dst2)
+
+    f = open(values_auto_file_name, "w")
+    for index, row in dataframe1.iterrows():
+        # process only the records with filled in 'Values'
+        if not isNaN(row['Values']) and isNotBlank(row['Values']):
+            param = row['Parameter Name'] + '  =   '
+            if(row['Data Type'] == 'string'):
+                param = param + '"' + row['Values'] + '"'
+                print(param)
+                f.writelines(param + "\n")
+            elif(row['Data Type'] == 'map(string)ttttt'):
+                mapStringList = row['Values'].strip()[1:-1].split('\n')
+                for item in mapStringList:
+                    if '=' in item:
+                        param = row['Parameter Name'] + '_' + item.strip()
+                        print(param)
+                        f.writelines(param + "\n")
+            elif(row['Data Type'] == 'map(string)'):
+                param = param + '"' + row['Values'] + '"'
+                print(param)
+                f.writelines(param + "\n")
+            else:
+                param = param + row['Values']
+                print(param)
+                f.writelines(param + "\n")
+    f.close()
+    # execute the terraform commands here.
+    os.system('echo terraform\terraform int Terraform commands here')
+
 print ("main.py completed...")
